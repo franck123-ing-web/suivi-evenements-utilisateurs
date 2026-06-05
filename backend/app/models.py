aadeb5cf1f975adb121d403e63f157a832f45c08
gama8speed <franck-joel.nzokou-takouju@estiam.com>
2026-06-05 09:41:42 +0200

    Création du modèle de données des événements

---
+++ backend/app/models.py
@@ -0,0 +1,38 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import DateTime
+from sqlalchemy import JSON
+
+from .database import Base
+
+class Event(Base):
+    __tablename__ = "events"
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
+
+    user_id = Column(
+        String(100),
+        nullable=False,
+        index=True
+    )
+
+    event_type = Column(
+        String(50),
+        nullable=False,
+        index=True
+    )
+
+    timestamp = Column(
+        DateTime,
+        nullable=False,
+        index=True
+    )
+
+    metadata = Column(
+        JSON,
+        nullable=True
+    )
