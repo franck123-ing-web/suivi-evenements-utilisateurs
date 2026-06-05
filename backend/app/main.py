2cf977cae0d714f08c11419fae9ddf0d6beee544
gama8speed <franck-joel.nzokou-takouju@estiam.com>
2026-06-05 11:44:39 +0200

    Correction des erreurs CORS et validation des requêtes API

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,19 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from .database import Base
-from .database import engine
-
+from .database import Base, engine
 from .routes.events import router
 
-Base.metadata.create_all(
-    bind=engine
-)
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Suivi des événements utilisateurs",
     version="1.0.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Routes
 app.include_router(router)
 
 
